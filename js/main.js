d88009a167c85268746531a286aa909c8eb989ea
theo <thoe0510@gmail.com>
2021-01-25 13:00:36 +0800

    backup

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function () {
+
+//    var prevScrollpos = window.pageYOffset;
+//    window.onscroll = function () {
+//        var currentScrollPos = window.pageYOffset;
+//        if (prevScrollpos > currentScrollPos) {
+//            $(".nav").css("top", "0");
+//        } else {
+//            $(".nav").css("top", "-50px");
+//        }
+//        prevScrollpos = currentScrollPos;
+//    }
+
+});
